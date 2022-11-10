9a99b47dfdc3266247e37ba857b0de84f06381e4
leoyanzon <leoyanzon@gmail.com>
2022-11-10 19:55:07 -0300

    Desafio N°6 WebSockets

--- app.js
+++ app.js
@@ -1,34 +1,39 @@
-const express = require("express");
-require('dotenv').config();
-const _=require("lodash");
+const express = require("express");   //Necesario para express
+const {Server: HttpServer} = require("http");
+const {Server: IoServer} = require("socket.io")
+require('dotenv').config(); //Declaración para uso de .env
+const _=require("lodash");  //Declaración para usar librería de chequeo de entradas
+const path = require('path');
+var morgan = require('morgan')
 
-const indexRouter = require("./src/routes/index");
-const errorMiddleware = require("./src/middlewares/errorMiddleware")
+const indexRouter = require("./src/routes/index"); //Declaración para uso de index.html
+const errorMiddleware = require("./src/middlewares/errorMiddleware") //Declaración de middleware
 
+//Creación de app
 const app = express();
+
+const http = new HttpServer(app);
+const io = new IoServer(http);
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
+
+
 const products = [{title: "Ejemplo1", price:22, thumbnail: "https://cdn1.iconfinder.com/data/icons/city-flat-2/512/people_person_man_stand_men-512.png"}];
-app.get("/health", (_req,res) =>{
-    res.status(200).json({
-        success: true,
-        environment: process.env.ENVIRONMENT || "undefined",
-        health: "up"
-    })
-})
+const messages = [{message: "hello"}];
+
 //Desafios anteriores
 app.use("/api", indexRouter);
 app.use("/static",express.static(__dirname + "/public"));
-
+app.use(express.static(path.join(__dirname, 'public')))
 
 //Nuevo desafio con EJS
 app.set("views", "./views");
 app.set("view engine", "ejs");
 
 app.get("/", (_req,res)=>{
-    res.render("pages/form")
+    res.render("pages/form", {products})
 });
 
 app.get("/productos", (_req,res)=>{
@@ -45,4 +50,20 @@ app.post("/productos", (req, res) => {
     }
 })
 
-module.exports = app;
+io.on("connection", (socket) => {
+    console.info("Nuevo cliente conectado");
+    socket.emit("UPDATE_DATA", messages);
+    socket.emit("UPDATE_PRODUCTS", products);
+    socket.on("NEW_MESSAGE_TO_SERVER", data => {
+        messages.push(data);
+        console.info(messages);
+        io.sockets.emit("NEW_MESSAGE_FROM_SERVER", data);
+    })
+    socket.on("NEW_PRODUCT_TO_SERVER", data => {
+        products.push(data);
+        console.info(products);
+        io.sockets.emit("NEW_PRODUCTS_FROM_SERVER", data);
+    })
+})
+
+module.exports = {app, http, io};
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 //Express app
-const app = require("./app");
+const { app , http, io} = require("./app");
 
 //ENV
 const PORT = process.env.PORT || 8080;
@@ -7,6 +7,15 @@ const PORT = process.env.PORT || 8080;
 //DB
 //const { Contenedor } = require("./desafio2");
 
-const server = app.listen(PORT, ()=>{
+const server = http.listen(PORT, ()=>{
     console.log(`Server up and running on port: ${server.address().port}`)
 })
+
+app.get("/health", (_req,res) =>{
+    res.status(200).json({
+        success: true,
+        environment: process.env.ENVIRONMENT || "undefined",
+        health: "up"
+    })
+})
+
